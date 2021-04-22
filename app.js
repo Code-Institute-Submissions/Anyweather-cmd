60c4955d2003c05388e3c8a377bb186e78e032d3
PaulSweeney-cmd <paulsweeney474@gmail.com>
2021-04-22 20:46:40 +0000

    Docs: Added further commenting

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let weather = {
-    apiKey: "28b8586750aa9252b6c5a344821cb9ff",
+    apiKey: "6a4db19a52703903602ea14236522a12",
 
     /*Creating the function to retrieve new weather data */
     getWeather: function (city) {
 
         /* Obtaining the unit meausurements & location from the source site using the API key provided */
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
             /* Then getting the response in json format */
             .then((response) => response.json())
 
@@ -22,7 +22,6 @@ let weather = {
 
         /* Retrieving the weather icon & conditions (or description) from the json data */
         const {
-            icon,
             description
         } = data.weather[0];
 
@@ -37,14 +36,49 @@ let weather = {
             speed
         } = data.wind;
 
-        console.log(name, icon, description, temp, humidity, speed);
-
         /* Assigning the variables above with their values from json to the HTML classes */
         document.querySelector(".location").innerText = name;
         document.querySelector(".conditions").innerText = "Conditions : " + description;
-        document.querySelector(".temperature-c").innerText = temp + "°c";
+        document.querySelector('#temperature').innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity : " + humidity + "%";
         document.querySelector(".wind-speed").innerText = "Wind Speed : " + speed + "mph";
+    },
 
+    /* Linking api data to a function and assigning to the search bar value */
+    search: function () {
+        this.getWeather(document.querySelector(".search-bar").value);
     }
+
 };
+
+
+
+/* JS for unresponsive unit conversion code discussed with tutor support*/
+function toFahrenheit() {
+
+    let convert = document.getElementById('temperature').textContent;
+    convert = convert * 9 / 5 + 32;
+    let splitVals = convert.split("°C");
+    splitVals = splitVals;
+    return convert;
+};
+/* JS for unresponsive unit conversion - end */
+
+
+
+document.querySelector(".enter-button")
+
+    /* Adding an event listener with a function to the enter button to retrieve api data when data assigned to the weather variable function is executed */
+    .addEventListener("click", function () {
+        weather.search();
+    });
+
+
+document.querySelector(".search-bar")
+
+    /* Adding a key up event listener so the user can press enter to retrieve weather instead of clicking on the button */
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
